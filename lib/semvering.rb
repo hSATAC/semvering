96afe6d776511dd4103be0a3a425f1d4a5d4ff02
hSATAC <hsatac@gmail.com>
2017-12-06 22:33:53 +0800

    Add basic comparision

--- lib/semvering.rb
+++ lib/semvering.rb
@@ -1,5 +1,41 @@
 require "semvering/version"
 
-module Semvering
-  # Your code goes here...
+class Semvering
+  include Comparable
+
+  attr :major, :minor, :patch
+  def initialize(major, minor, patch)
+    raise ArgumentError, "version number should not be non-negative integers" if major < 0 || minor < 0 || patch < 0
+
+    @major = major
+    @minor = minor
+    @patch = patch
+  end
+
+  def <=>(another)
+    case @major <=> another.major
+    when 1
+      return 1
+    when -1
+      return -1
+    when 0
+      case @minor <=> another.minor
+      when 1
+        return 1
+      when -1
+        return -1
+      when 0
+        return @patch <=> another.patch
+      end
+    end
+    #if @major > another.major
+      #return 1
+    #elsif @major < another.major
+      #return -1
+    #else
+      #if @minor > another.minor
+        #return 1
+      #end
+    #end
+  end
 end
--- spec/semvering_spec.rb
+++ spec/semvering_spec.rb
@@ -1,11 +1,84 @@
 require "spec_helper"
+# https://semver.org/
 
 RSpec.describe Semvering do
   it "has a version number" do
     expect(Semvering::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  context "#new" do
+    it "take the form X.Y.Z" do
+      ver = Semvering.new 1, 1, 1
+      expect(ver.class).to eq(Semvering)
+    end
+
+    it "raise ArgumentError when X, Y, Z are negative integers" do
+      expect{ Semvering.new(-1, 0, 0)}.to raise_error(ArgumentError)
+      expect{ Semvering.new(0, -1, 0)}.to raise_error(ArgumentError)
+      expect{ Semvering.new(0, 0, -1)}.to raise_error(ArgumentError)
+    end
+  end
+
+  context "comparision" do
+    describe ">" do
+      it "compares the major number" do
+        ver1 = Semvering.new 1, 0, 0
+        ver2 = Semvering.new 2, 0, 0
+
+        expect( ver2 > ver1 ).to be true
+      end
+
+      it "compares the minor number" do
+        ver1 = Semvering.new 1, 1, 0
+        ver2 = Semvering.new 1, 2, 0
+        expect( ver2 > ver1 ).to be true
+      end
+
+      it "compares the patch number" do
+        ver1 = Semvering.new 1, 0, 1
+        ver2 = Semvering.new 1, 0, 2
+        expect( ver2 > ver1 ).to be true
+      end
+    end
+
+    describe "<" do
+      it "compares the major number" do
+        ver1 = Semvering.new 1, 0, 0
+        ver2 = Semvering.new 2, 0, 0
+
+        expect( ver1 < ver2 ).to be true
+      end
+
+      it "compares the minor number" do
+        ver1 = Semvering.new 1, 1, 0
+        ver2 = Semvering.new 1, 2, 0
+        expect( ver1 < ver2 ).to be true
+      end
+
+      it "compares the patch number" do
+        ver1 = Semvering.new 1, 0, 1
+        ver2 = Semvering.new 1, 0, 2
+        expect( ver1 < ver2 ).to be true
+      end
+    end
+
+    describe "==" do
+      it "compares all numbers" do
+        ver1 = Semvering.new 1, 0, 0
+        ver2 = Semvering.new 1, 0, 0
+
+        expect( ver1 == ver2 ).to be true
+      end
+    end
+
+    describe "!=" do
+      it "compares all numbers" do
+        ver1 = Semvering.new 1, 0, 0
+        ver2 = Semvering.new 1, 0, 0
+
+        expect( ver1 != ver2 ).to be false
+      end
+    end
+
   end
 end
