412aa0b847f02b386495f8e3337a6b4e446c7f5b
hSATAC <hsatac@gmail.com>
2017-12-07 00:06:27 +0800

    Add basic pre-release comparable

--- lib/semvering.rb
+++ lib/semvering.rb
@@ -3,13 +3,15 @@ require "semvering/version"
 class Semvering
   include Comparable
 
-  attr :major, :minor, :patch
-  def initialize(major, minor, patch)
-    raise ArgumentError, "version number should not be non-negative integers" if major < 0 || minor < 0 || patch < 0
+  attr :major, :minor, :patch, :pre_release
+  def initialize(major, minor, patch, pre_release: nil)
+    raise ArgumentError, "version number should not be non-negative integers" unless check_version_format major, minor, patch
+    raise ArgumentError, "pre_release verson should only contain alphanumerics" unless check_pre_release_format pre_release
 
     @major = major
     @minor = minor
     @patch = patch
+    @pre_release = pre_release
   end
 
   def <=>(another)
@@ -21,8 +23,29 @@ class Semvering
       when 1, -1
         return res
       when 0
-        return @patch <=> another.patch
+        case res = @patch <=> another.patch
+        when 1, -1
+          return res
+        when 0
+          return 0 if @pre_release == another.pre_release
+          return 1 if @pre_release == nil && another.pre_release != nil
+          return -1 if @pre_release != nil && another.pre_release == nil
+          # TODO: Compare between different pre_release
+        end
       end
     end
+  end
+
+  private
+  def check_version_format(major, minor, patch)
+    major >= 0 && minor >= 0 && patch >= 0
+  end
+
+  def check_pre_release_format(pre_release)
+    return true if pre_release.nil?
+    return false if (pre_release =~ /^[0-9A-Za-z\-.]+$/).nil?
+    return false if pre_release.scan(/\d+/).any?{ |n| n.to_i.to_s != n }
+
+    return true
   end
 end
--- spec/semvering_spec.rb
+++ spec/semvering_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Semvering do
     expect(Semvering::VERSION).not_to be nil
   end
 
-  context "#new" do
+  describe "#new" do
     it "take the form X.Y.Z" do
       ver = Semvering.new 1, 1, 1
       expect(ver.class).to eq(Semvering)
@@ -17,10 +17,23 @@ RSpec.describe Semvering do
       expect{ Semvering.new(0, -1, 0)}.to raise_error(ArgumentError)
       expect{ Semvering.new(0, 0, -1)}.to raise_error(ArgumentError)
     end
+
+    it "take the pre-release version" do
+      ver = Semvering.new 1, 1, 1, pre_release: "alpha.0.1"
+      expect(ver.class).to eq(Semvering)
+    end
+
+    it "raise ArgumentError when pre-release does not fit the format" do
+      expect{ Semvering.new 1, 1, 1, pre_release: "0.01%.0" }.to raise_error(ArgumentError)
+    end
+
+    it "raise ArgumentError when pre-release contains leading zeroes numeric identifier" do
+      expect{ Semvering.new 1, 1, 1, pre_release: "0.01.0" }.to raise_error(ArgumentError)
+    end
   end
 
-  context "comparision" do
-    describe ">" do
+  describe "comparable" do
+    context " simple x, y, z" do
       it "compares the major number" do
         ver1 = Semvering.new 1, 0, 0
         ver2 = Semvering.new 2, 0, 0
@@ -38,42 +51,31 @@ RSpec.describe Semvering do
         ver2 = Semvering.new 1, 0, 2
         expect( ver2 > ver1 ).to be true
       end
-    end
 
-    describe "<" do
-      it "compares the major number" do
-        ver1 = Semvering.new 1, 0, 0
-        ver2 = Semvering.new 2, 0, 0
-        expect( ver1 < ver2 ).to be true
-      end
-
-      it "compares the minor number" do
-        ver1 = Semvering.new 1, 1, 0
-        ver2 = Semvering.new 1, 2, 0
-        expect( ver1 < ver2 ).to be true
-      end
-
-      it "compares the patch number" do
-        ver1 = Semvering.new 1, 0, 1
-        ver2 = Semvering.new 1, 0, 2
-        expect( ver1 < ver2 ).to be true
-      end
-    end
-
-    describe "==" do
-      it "compares all numbers" do
+      it "compares equal version" do
         ver1 = Semvering.new 1, 0, 0
         ver2 = Semvering.new 1, 0, 0
         expect( ver1 == ver2 ).to be true
       end
-    end
 
-    describe "!=" do
-      it "compares all properties" do
+      it "compares inequal version" do
         ver1 = Semvering.new 1, 0, 0
         expect( ver1 != ver1 ).to be false
       end
     end
+
+    context "pre-release comparable" do
+      it "pre-release version has a lower precedence than normal version" do
+        ver1 = Semvering.new 1, 0, 0, pre_release: "alpha.1"
+        ver2 = Semvering.new 1, 0, 0
+        expect( ver2 > ver1 ).to be true
+        expect( ver2 < ver1 ).to be false
+        expect( ver2 == ver1 ).to be false
+      end
+
+      it "compare between pre-release versions"
+    end
+
 
   end
 end
